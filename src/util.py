5a3ff0ea138771032f2438244cb4e36edaa8a899
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-03-22 08:24:13 -0400

    Fixed potential bug in count_matches algorithm

--- src/util.py
+++ src/util.py
@@ -24,14 +24,14 @@ def count_matches(x, y):
     x_values = list(x.values())
     y_keys = list(y.keys())
     y_values = list(y.values())
-    if len(x) < len(y):
-        for i in range(len(x_keys)):
+    if len(x) > len(y):
+        for i in range(len(y_keys)):
             if math.isclose(list_avg(x_values[i]), list_avg(y_values[i])):
                 matching_value_count += 1
-        return matching_value_count
-    for i in range(len(y_keys)):
-        if x_keys[i] == y_keys[i] and math.isclose(
-            list_avg(x_values[i]), list_avg(y_values[i])
-        ):
+            return matching_value_count
+    # This case is only reached if the len(y) > len(x). We use the length of x_keys to iterate because otherwise we will
+    # go outside the array bounds
+    for i in range(len(x_keys)):
+        if math.isclose(list_avg(x_values[i]), list_avg(y_values[i])):
             matching_value_count += 1
     return matching_value_count
