af1ee33266ed95acd7455c3dedeed96ee263523f
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-03-28 09:56:20 -0400

    Plot KIT values
    Doesn't look very helpful so maybe we cam use something else

--- src/detector.py
+++ src/detector.py
@@ -1,9 +1,11 @@
 from tqdm import tqdm
 import os
+import sys
 
 from kht import get_all_users_features_KHT, get_all_users_features_KHT_from_file
 from kit import get_all_users_features_KIT
 from util import count_matches, pretty_print, list_avg
+from plotter import make_line_graph
 
 if __name__ == "__main__":
     dir_name = "data"
@@ -14,20 +16,32 @@ if __name__ == "__main__":
     # selected_profile_KHT_dictionary = get_all_users_features_KHT_from_file(
     #     selected_profile_path
     # )
-    # user_files = os.listdir(os.path.join(dir_path, "Desktop/"))
+    user_files = os.listdir(os.path.join(dir_path, "Desktop/"))
     # # pretty_print(get_all_users_features_KHT(user_files))
     # # pretty_print(get_all_users_features_KIT(user_files)[0])
-    # for i in tqdm(range(len(user_files))):
-    #     user_file = user_files[i]
-    #     current_KHT_dict = get_all_users_features_KHT_from_file(
-    #         os.path.join(dir_path, "Desktop/", user_files[i])
-    #     )
-    #     print("Running against profile: %s" % user_file)
-    #     k, v = count_matches(selected_profile_KHT_dictionary, current_KHT_dict)
-    #     print("Found", k, "key matches")
-    #     print("Found", v, "value matches")
-
+    for i in tqdm(range(len(user_files))):
+        user_file = user_files[i]
+        # current_KHT_dict = get_all_users_features_KHT_from_file(
+        #     os.path.join(dir_path, "Desktop/", user_files[i])
+        # )
+        #     print("Running against profile: %s" % user_file)
+        # k, v = count_matches(selected_profile_KHT_dictionary, current_KHT_dict)
+        #     print("Found", k, "key matches")
+        #     print("Found", v, "value matches")
+        # make_histogram(current_KHT_dict)
     # The KIT version
     profiles = get_all_users_features_KIT(
         os.path.join(dir_path, "Desktop/"), selected_profile_path
     )
+    processed_KIT = {}
+    current_KIT = profiles[0]
+    KIT_values = list(current_KIT.values())
+    KIT_keys = list(current_KIT.keys())
+    for i in KIT_values:
+        vals = dict(i)
+        for k, v in vals.items():
+            processed_KIT[k] = list_avg(v)
+    # for i in range(len(KIT_keys)):
+    #     processed_KIT[KIT_keys[i]] = list_avg(KIT_values[i])
+    pretty_print(processed_KIT)
+    make_line_graph(processed_KIT)
---
+++ src/plotter.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+
+def make_line_graph(data: dict):
+    lists = sorted(data.items())  # sorted by key, return a list of tuples
+    x, y = zip(*lists)  # unpack a list of pairs into two tuples
+    plt.gca().xaxis.set_major_locator(MaxNLocator(prune="lower"))
+    plt.gca().yaxis.set_major_locator(MaxNLocator(prune="lower"))
+
+    plt.plot(x, y)
+    plt.show()
